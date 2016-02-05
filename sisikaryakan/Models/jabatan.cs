9c0224095d0a8cedbd00936f90eeb011b69a6de6
dwioktaviani <dwi.oktaviani@basajans.com>
2016-02-05 16:03:01 +0700

    update - rename field jabatan to namaJabatan

--- sisikaryakan/Models/jabatan.cs
+++ sisikaryakan/Models/jabatan.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +10,7 @@ namespace sisikaryakan.Models
     {
         public int kodeJabatan { get; set; }
 
-        public string jabatan { get; set; }
+        [Required]
+        public string namaJabatan { get; set; }
     }
 }
