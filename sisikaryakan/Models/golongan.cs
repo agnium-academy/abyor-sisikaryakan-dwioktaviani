6c43c6f68ecdf97b1aff2e026a1f1789a9254a1e
dwioktaviani <dwi.oktaviani@basajans.com>
2016-02-05 17:14:05 +0700

    upload file model class golongan

---
+++ sisikaryakan/Models/golongan.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace sisikaryakan.Models
+{
+    public class golongan
+    {
+        public int kodeGolongan { get; set; }
+
+        [Required]
+        public string namaGolongan { get; set; }
+
+        
+    }
+}
