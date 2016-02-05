782d779df30493a8e93c1d8a279816b5ffb8f487
dwioktaviani <dwi.oktaviani@basajans.com>
2016-02-05 16:07:27 +0700

    upload file model class dataPegawai

--- sisikaryakan/Models/dataPegawai.cs
+++ sisikaryakan/Models/dataPegawai.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Http;
 
 namespace sisikaryakan.Models
 {
@@ -9,14 +9,31 @@ namespace sisikaryakan.Models
     {
         public int NIK { get; set; }
 
+        [Required]
         public string namaPegawai { get; set; }
 
+        [Required]
         public string tempatLahir { get; set; }
 
+        [Required]
         public DateTime tanggalLahir { get; set; }
 
+        [Required]
         public string alamat { get; set; }
 
+        [Required]
         public Enum jenisKelamin { get; set; }
+
+        [Required]
+        public string noTelp { get; set; }
+
+        [Required]
+        public Enum status { get; set; }
+
+        [Required]
+        public jabatan kodeJabatan { get; set; }
+
+        [HttpBindNever]
+        public jabatan namaJabatan { get; set; }
     }
 }
