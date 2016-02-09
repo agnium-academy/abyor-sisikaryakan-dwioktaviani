3b6a3c91e1e7bac655021dea07e2973c56485604
dwioktaviani <dwi.oktaviani@basajans.com>
2016-02-09 11:53:28 +0700

    upload file tunjangan.cs to Model

---
+++ sisikaryakan/Models/tunjangan.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace sisikaryakan.Models
+{
+    public class tunjangan
+    {
+        public int kodeTunjuangan { get; set; }
+
+        [Required]
+        public string namaTunjangan { get; set; }
+
+        [Required]
+        public int biaya { get; set; }
+
+        [Required]
+        public golongan kodeGolongan { get; set; }
+
+        [HttpBindNever]
+        public golongan namaGolongan { get; set;  }
+
+
+    }
+}
